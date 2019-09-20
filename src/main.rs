1897988c34c49ce0a3d9af096c3d45ced612439b
Cass May <ingeniousmammal@gmail.com>
2019-09-20 23:51:25 +0100

    read midi input, identify notes by name, and print status

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,68 @@
+extern crate midir;
+
+use std::io::{stdin, stdout, Write};
+use std::error::Error;
+
+use midir::{MidiInput, Ignore};
+
 fn main() {
-    println!("Hello, world!");
+    match run() {
+        Ok(_) => (),
+        Err(err) => println!("Error: {}", err.description())
+    }
+}
+
+fn run() -> Result<(), Box<Error>> {
+    let mut input = String::new();
+
+    let mut midi_in = MidiInput::new("midir forwarding input")?;
+    midi_in.ignore(Ignore::None);
+
+    let in_port = match midi_in.port_count() {
+        0 => return Err("no input port found".into()),
+        1 => {
+            println!("Choosing the only available input port: {}", midi_in.port_name(0).unwrap());
+            0
+        },
+        _ => {
+            println!("\nAvailable input ports:");
+            for i in 0..midi_in.port_count() {
+                println!("{}: {}", i, midi_in.port_name(i).unwrap())
+            }
+            print!("Please select input port: ");
+            stdout().flush()?;
+            let mut input = String::new();
+            stdin().read_line(&mut input)?;
+            input.trim().parse::<usize>()?
+        }
+    };
+
+    println!("\nOpening connection");
+    let in_port_name = midi_in.port_name(in_port)?;
+
+    let _conn_in = midi_in.connect(in_port, "midir-read-input", move |stamp, message, _| {
+        process_msg(message);
+    }, ())?;
+
+    println!("Connection open, reading input from '{}' (press enter to exit) ...", in_port_name);
+
+    input.clear();
+    stdin().read_line(&mut input)?; // wait for next enter key press
+
+    println!("Closing connection");
+    Ok(())
+}
+
+fn process_msg(msg: &[u8]) {
+    let note_names = ["A", "A#", "B", "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#"];
+    let midi_start_index = 21;
+
+    let action = match msg[0] {
+        0x90 => "KeyDown",
+        0x80 => "KeyUp",
+        _ => "Unknown"
+    };
+    let note_index = msg[1] - midi_start_index;
+    let note = format!("{}{}", note_names[note_index as usize % note_names.len()], note_index / note_names.len() as u8);
+    println!("{}: {}", action, note);
 }
