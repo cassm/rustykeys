07dd91d413b69713ec69c27fb0f5c73969d51a7c
Cass May <ingeniousmammal@gmail.com>
2019-09-24 19:14:27 +0100

    Add scale practicing, sharp/flat differentiation, and color output

--- src/main.rs
+++ src/main.rs
@@ -6,20 +6,26 @@ extern crate timer;
 extern crate chrono;
 extern crate rand;
 extern crate dialoguer;
+extern crate termion;
 
 use ordinal::Ordinal;
 use std::sync::Mutex;
 use std::io::{stdin, stdout, Write};
 use std::error::Error;
 use std::fmt;
-use std::time::Instant;
+use std::time::{self, Instant};
+use std::thread;
 
 use midir::{MidiInput, Ignore};
 use rand::{thread_rng, seq::SliceRandom};
 use dialoguer::{theme::ColorfulTheme, Select, Confirmation};
+use termion::color;
 
-const NOTE_NAMES: &'static [&'static str] = &["A", "A#", "B", "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#"];
+
+const NOTE_NAMES: &'static [&'static [&'static str]] = &[&["A"], &["A#", "Bb"], &["B"], &["C"], &["C#", "Db"], &["D"], &["D#", "Eb"], &["E"], &["F"], &["F#", "Gb"], &["G"], &["G#", "Ab"]];
 const MAJOR_SCALE_INTERVALS: &'static [usize] = &[2, 2, 1, 2, 2, 2, 1];
+const DEBOUNCE_MILLIS: u64 = 100;
+
 
 lazy_static! {
     static ref KEYS_DOWN: Mutex<Vec<u8>> = Mutex::new(vec![]);
@@ -31,6 +37,17 @@ fn main() {
         Ok(_) => (),
         Err(err) => println!("Error: {}", err.description())
     }
+}
+
+
+enum Mode {
+        Ionian = 0,
+        Dorian = 1,
+        Phrygian = 2,
+        Lydian = 3,
+        Mixolydian = 4,
+        Aeolian = 5,
+        Locrian = 6,
 }
 
 #[derive(Copy, Clone, PartialEq, Eq)]
@@ -145,7 +162,7 @@ fn run() -> Result<(), Box<dyn Error>> {
         .unwrap()
     {
         0 => practice_chords_launcher(),
-        1 => generate_scale(),
+        1 => practice_scales_launcher(),
         _ => Ok(()),
     }
 }
@@ -226,12 +243,12 @@ fn generate_chord_list(chord_type: ChordType, inversion: usize, hand: Hand) -> V
     let mut chords: Vec<(Chord, Hand)> = vec!();
 
     if hand == Hand::Left || hand == Hand::Both {
-        let chords_to_add: Vec<(Chord, Hand)> = NOTE_NAMES.iter().map(|x| (Chord{root: x.to_string(), chord_type, inversion, octave: None}, Hand::Left)).collect();
+        let chords_to_add: Vec<(Chord, Hand)> = NOTE_NAMES.iter().map(|x| (Chord{root: x[0].to_string(), chord_type, inversion, octave: None}, Hand::Left)).collect();
         chords.extend(chords_to_add);
 
     }
     if hand == Hand::Right || hand == Hand::Both {
-        let chords_to_add: Vec<(Chord, Hand)> = NOTE_NAMES.iter().map(|x| (Chord{root: x.to_string(), chord_type, inversion, octave: None}, Hand::Right)).collect();
+        let chords_to_add: Vec<(Chord, Hand)> = NOTE_NAMES.iter().map(|x| (Chord{root: x[0].to_string(), chord_type, inversion, octave: None}, Hand::Right)).collect();
         chords.extend(chords_to_add);
     }
 
@@ -251,8 +268,6 @@ fn practice_chords(chord_type: ChordType, inversion: usize, hand: Hand) -> Resul
     let in_port_name = midi_in.port_name(in_port)?;
 
     println!("\nOpening connection...");
-
-    let debounce_millis = 100;
 
     let _conn_in = midi_in.connect(in_port, "midir-read-input", move |_, message, _| {
         *LAST_KEY_PRESS.lock().unwrap() = Some(Instant::now());
@@ -274,31 +289,34 @@ fn practice_chords(chord_type: ChordType, inversion: usize, hand: Hand) -> Resul
 
             match last_key_press {
                 Some(i) => {
-                    if i.elapsed().as_millis() > debounce_millis {
+                    if i.elapsed().as_millis() > DEBOUNCE_MILLIS.into() {
                         *LAST_KEY_PRESS.lock().unwrap() = None;
                         if KEYS_DOWN.lock().unwrap().len() > 0 {
                             match identify_chord() {
                                 Some(i) => {
-                                    println!("chord: {}", i);
-
                                     let octave_match = match i.octave {
                                         Some(j) => chords[0].1.value().contains(&j),
                                         None => true,
                                     };
 
                                     if chords[0].0 == i && octave_match {
-                                        println!("Correct!");
+                                        println!("{}Correct!{}", color::Fg(color::Green), color::Fg(color::Reset));
                                         chords.remove(0);
+                                        thread::sleep(time::Duration::from_millis(DEBOUNCE_MILLIS));
 
                                         if chords.len() > 0 {
                                             println!("Play {}, {}", chords[0].0, chords[0].1);
                                         }
                                     }
                                     else {
-                                        println!("Try again: {}, {}", chords[0].0, chords[0].1);
+                                        println!("{}Try again: {}, {}{}", color::Fg(color::Red), chords[0].0, chords[0].1, color::Fg(color::Reset));
+                                        thread::sleep(time::Duration::from_millis(DEBOUNCE_MILLIS));
                                     }
                                 },
-                                None => println!("unrecognised chord\nTry again: {}, {}", chords[0].0, chords[0].1),
+                                None => {
+                                    println!("{}unrecognised chord\nTry again: {}, {}{}", color::Fg(color::Red), chords[0].0, chords[0].1, color::Fg(color::Reset));
+                                    thread::sleep(time::Duration::from_millis(DEBOUNCE_MILLIS));
+                                }
                             }
                         }
                     }
@@ -329,7 +347,15 @@ fn get_note_name(key_index: u8) -> String {
 
     let note_index = key_index - midi_start_index;
 
-    NOTE_NAMES[note_index as usize % NOTE_NAMES.len()].to_string()
+    NOTE_NAMES[note_index as usize % NOTE_NAMES.len()][0].to_string()
+}
+
+fn note_matches(key_index: u8, note: &str) -> bool {
+    let midi_start_index = 21;
+
+    let note_index = key_index - midi_start_index;
+
+    return NOTE_NAMES[note_index as usize % NOTE_NAMES.len()].contains(&note);
 }
 
 fn process_msg(msg: &[u8]) {
@@ -389,41 +415,114 @@ fn identify_chord() -> Option<Chord> {
     return None
 }
 
-fn generate_scale() -> Result<(), Box<dyn Error>> {
-    let scale_types: &'static [&'static str] = &[
-        "I - Ionian (major scale)",
-        "II - Dorian (a minor scale with a sharp 6th, gives it a bit of a jazzy/upbeat vibe)",
-        "III - Phrygian (very common in metal or spanish classical. Really brooding, depressing mode)",
-        "IV - Lydian (Kind of spacey and dreamy, like a major scale that is tripping)",
-        "V - Mixolydian (bluesy)",
-        "VI - Aeolian (minor scale, omnipresent in music)",
-        "VII - Locrian (rarely used)",
-    ];
+fn practice_scales_launcher() -> Result<(), Box<dyn Error>> {
+    let mut midi_in = MidiInput::new("midir forwarding input")?;
+    midi_in.ignore(Ignore::None);
 
-    let root = Select::with_theme(&ColorfulTheme::default())
+    let in_port = match get_in_port(&midi_in) {
+        Ok(i)  => i,
+        Err(e) => return Err(e),
+    };
+
+    let in_port_name = midi_in.port_name(in_port)?;
+
+    println!("\nOpening connection...");
+
+    let _conn_in = midi_in.connect(in_port, "midir-read-input", move |_, message, _| {
+        *LAST_KEY_PRESS.lock().unwrap() = Some(Instant::now());
+        process_msg(message);
+    }, ())?;
+
+    println!("Connection open, reading input from '{}'. Press ^C to quit\n", in_port_name);
+
+    let mut replay = true;
+
+    while replay {
+        let mut scale = generate_scale();
+
+        while scale.len() > 0 {
+            let last_key_press = *LAST_KEY_PRESS.lock().unwrap();
+
+            match last_key_press {
+                Some(_) => {
+                    *LAST_KEY_PRESS.lock().unwrap() = None;
+
+                    if let Some(i) = KEYS_DOWN.lock().unwrap().last() {
+                        if note_matches(*i, &scale[0]) {
+                            // delete incorrect input
+                            print!("{}{}{} ", color::Fg(color::Green), scale[0], color::Fg(color::Reset));
+                            stdout().flush()?;
+                            scale.remove(0);
+                            thread::sleep(time::Duration::from_millis(DEBOUNCE_MILLIS));
+                        }
+                        else {
+                            print!("{}{}{} ", color::Fg(color::Red), get_note_name(*i), color::Fg(color::Reset));
+                            stdout().flush()?;
+                            thread::sleep(time::Duration::from_millis(DEBOUNCE_MILLIS));
+                        }
+
+                    }
+                }
+                _ => {},
+            }
+        }
+        println!("");
+
+        replay = Confirmation::new()
+            .with_text("Would you like to practice again?")
+            .interact()
+            .unwrap();
+    }
+    Ok(())
+}
+
+fn generate_scale() -> Vec<String> {
+    let modes: &'static [&'static str] = &[
+        "I   - Ionian (major scale)",
+        "II  - Dorian (a minor scale with a sharp 6th, gives it a bit of a jazzy/upbeat vibe)",
+        "III - Phrygian (very common in metal or spanish classical. Really brooding, depressing mode)",
+        "IV  - Lydian (Kind of spacey and dreamy, like a major scale that is tripping)",
+        "V   - Mixolydian (bluesy)",
+        "VI  - Aeolian (minor scale, omnipresent in music)",
+        "VII - Locrian (rarely used)",
+     ];
+
+    let note_names: Vec<&str> = NOTE_NAMES.iter().map(|x| x[0]).collect();
+
+    let root_index = Select::with_theme(&ColorfulTheme::default())
         .with_prompt("Pick a root note")
-        .items(NOTE_NAMES)
+        .items(note_names.as_slice())
         .interact()
         .unwrap();
 
-    let scale_type = Select::with_theme(&ColorfulTheme::default())
+    let mode_offset = Select::with_theme(&ColorfulTheme::default())
         .with_prompt("Pick a scale type")
-        .items(scale_types)
+        .items(modes)
         .interact()
         .unwrap();
 
     let mut scale: Vec<String> = vec!();
     let mut offset: usize = 0;
 
-    for i in 0..MAJOR_SCALE_INTERVALS.len() {
-        let note_index = (root + offset) % NOTE_NAMES.len();
-        scale.push(NOTE_NAMES[note_index].to_string());
+    for i in 0..MAJOR_SCALE_INTERVALS.len() + 1 {
+        let note_index = (root_index + offset) % NOTE_NAMES.len();
 
-        let interval_index = (i + scale_type) % MAJOR_SCALE_INTERVALS.len();
+        // use the correct sharp or flat
+        if let Some(j) = scale.last() {
+            if NOTE_NAMES[note_index].len() > 1 && NOTE_NAMES[note_index][0][..1] == j[..1] {
+                scale.push(NOTE_NAMES[note_index][1].to_string());
+            }
+            else {
+                scale.push(NOTE_NAMES[note_index][0].to_string());
+            }
+        }
+        else {
+            scale.push(NOTE_NAMES[note_index][0].to_string());
+        }
+
+        let interval_index = (i + mode_offset) % MAJOR_SCALE_INTERVALS.len();
         offset += MAJOR_SCALE_INTERVALS[interval_index];
     }
 
-    println!("{:?}", scale);
-
-    Ok(())
+    scale
 }
